ab463b47c3354ae61cd190967c09d8b6c93763db
Tobias Hehrlein <tobias.hehrlein@appsfactory.de>
2020-10-05 12:12:15 +0200

    [REFACTOR] spacing

--- ui_block/src/main/java/com/tobiapplications/fahrstuhlblock/ui_block/input/BlockInputAdapter.kt
+++ ui_block/src/main/java/com/tobiapplications/fahrstuhlblock/ui_block/input/BlockInputAdapter.kt
@@ -80,7 +80,6 @@ class BlockInputAdapter(
             binding.executeAfter {
                 this.item = inputDataItem
             }
-
             interactions.onInputChanged()
         }
     }
